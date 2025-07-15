4dd953539c2ae3dc30d9d7212b88dbdaca12f48b
lintangtimur <lintangtimur915@gmail.com>
2025-07-15 14:33:45 +0900

    tbale migrations

---
+++ database/migrations/2025_07_15_051015_create_item_transactions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_transactions', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->bigInteger('item_id');
+            $table->string('transaction_type',10);
+            $table->integer('qty');
+            $table->dateTime('transaction_date');
+            $table->string('source');
+            $table->text('note')->nullable();
+            $table->bigInteger('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_transactions');
+    }
+};
