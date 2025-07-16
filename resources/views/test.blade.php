ee3dcb7ff1d9e519965cfb509ec5cc08497eeed9
lintangtimur <lintangtimur915@gmail.com>
2025-07-16 13:45:25 +0900

    test route

---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('content-header')
+    HALMAAN LINTANG
+@endsection
+@section('content')
+    test lintang
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UnitController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test');
 });
 
 
